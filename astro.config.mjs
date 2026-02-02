83d66896fdc0a8d3d3bc1d2edbafbcbf21d47494
abeynavarro <abeynavarro@gmail.com>
2026-02-02 13:17:04 +0100

    Eliminar importaciones no utilizadas de remarkToc y rehypeAccessibleEmojis en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import remarkToc from 'remark-toc';
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 // https://astro.build/config
 export default defineConfig({
