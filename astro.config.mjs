187de83a2bfd3a7cc3d8a77c3fb1d6b40f6e5a8f
abeynavarro <abeynavarro@gmail.com>
2026-02-02 12:45:30 +0100

    Incluidos plugin remarkToc y Emojis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkToc from 'remark-toc';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 
 // https://astro.build/config
 export default defineConfig({
